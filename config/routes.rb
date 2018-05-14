bc3a74703eacf02768cd18b637fd648b5ddce172
efsa <mit@efsa.unsa.ba>
2018-05-14 13:47:22 +0200

    navbar
    dodavanje navbara

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   
  root 'pages#landing_page'
-
-
-
- get '/second_page' => 'pages#second_page'
+ get '/druga' => 'pages#druga'
+ get '/register_login' => 'pages#register_login'
+ get '/proizvodi' => 'pages#proizvodi'
 
 end
